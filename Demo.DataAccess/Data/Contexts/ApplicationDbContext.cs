16575240130f636be161ca0fd71f5a731d293b5d
Ali Ewida <Ali Ewida@ALI_TUF>
2025-09-22 00:03:42 +0300

    Department [Entities - Configrations]

---
+++ Demo.DataAccess/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Principal;
+
+namespace Demo.DataAccess.Data.Contexts
+{
+    internal class ApplicationDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Connection String");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
---
+++ Demo.DataAccess/Models/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo.DataAccess.Models
+{
+    internal class BaseEntity // Include Common Properties [Parent]
+    {
+        public int Id { get; set; }
+        public int CreatedBy { get; set; }  // User Id
+        public DateTime? CreatedOn { get; set; }  // The Date Time Of Creating the Record
+        public int ModifiedBy { get; set; }  // User Id
+        public DateTime? ModifiedOn { get; set; }  // The Date Time Of Modifing the Record
+        public bool IsDeleted { get; set; } // SoftDelete
+    }
+}
