bb174b77e4c2311f02cba9fb4568f4a3ab353478
Ali Ewida <Ali Ewida@ALI_TUF>
2025-09-24 16:22:17 +0300

    Department - Create

--- Demo.BusinessLogic/DTOS/CreatedepartmentDto.cs
+++ Demo.BusinessLogic/DTOS/CreatedepartmentDto.cs
@@ -1,10 +1,16 @@
-﻿namespace Demo.BusinessLogic.DTOS
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo.BusinessLogic.DTOS
 {
     public class CreatedepartmentDto
     {
+        [Required] // Name Is Required
         public string Name { get; set; }=string.Empty;
+
+        [Required(ErrorMessage = " Code Is Required !!")]
+        public string Code { get; set; } = string.Empty;
         public string? Description { get; set; }
-        public string Code { get; set; }= string.Empty;
+
         public DateOnly DateOfCreation { get; set; }
     }
 }
--- Demo.Presentation/Controllers/DepartmentController.cs
+++ Demo.Presentation/Controllers/DepartmentController.cs
@@ -1,15 +1,67 @@
-﻿using Demo.BusinessLogic.Services.Classes;
+﻿using Demo.BusinessLogic.DTOS;
+using Demo.BusinessLogic.Services.Classes;
 using Demo.BusinessLogic.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
 
 namespace Demo.Presentation.Controllers
 {
-    public class DepartmentController(IDepartmentService _departmentService) : Controller
+    public class DepartmentController(IDepartmentService _departmentService
+        , IWebHostEnvironment _env, ILogger<DepartmentController> _logger) : Controller
     {
+        #region Index
+        [HttpGet]
         public IActionResult Index()
         {
             var departments = _departmentService.GetAllDepartments();
             return View(departments);
         }
+        #endregion
+
+        #region Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreatedepartmentDto departmentDto)
+        {
+            if (ModelState.IsValid) // Server Side Validation
+            {
+                try
+                {
+                    int result = _departmentService.AddDepartment(departmentDto);
+                    if (result > 0)
+                    {
+                       return RedirectToAction(nameof(Index));    
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Department Can not be Created !!");
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (_env.IsDevelopment())
+                    {
+                        _logger.LogError($"Department Con not be created becouse : {ex.Message}");
+
+                    }
+                    else
+                    {
+                        _logger.LogError($"Department Con not be created becouse : {ex.Message}");
+                        //return View(departmentDto);
+                        return View("Error view");
+                    }
+                }
+            }
+            return View(departmentDto);
+
+        }
+        #endregion
     }
 }
+
