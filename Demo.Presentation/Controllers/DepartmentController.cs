0401aa26e6414e97b3601f38e00f6b98ca1651c0
Ali Ewida <Ali Ewida@ALI_TUF>
2025-09-24 17:17:08 +0300

    Department - Details

--- Demo.Presentation/Controllers/DepartmentController.cs
+++ Demo.Presentation/Controllers/DepartmentController.cs
@@ -62,6 +62,17 @@ namespace Demo.Presentation.Controllers
 
         }
         #endregion
+
+        #region Details
+        [HttpGet]
+        public IActionResult Details(int? id)
+        { 
+            if(!id.HasValue) return BadRequest();
+            var department = _departmentService.GetDepartmentById(id.Value);
+            if (department is null) return NotFound();
+            return View(department);
+        }
+        #endregion
     }
 }
 
