b9fa17a78700970a4b3b7e5b62aeb6d1895f6b0f
Ali Ewida <Ali Ewida@ALI_TUF>
2025-09-27 15:00:21 +0300

    Employee - Details

--- Demo.Presentation/Controllers/EmployeeController.cs
+++ Demo.Presentation/Controllers/EmployeeController.cs
@@ -1,9 +1,13 @@
-﻿using Demo.BusinessLogic.Services.Interfaces;
+﻿using Demo.BusinessLogic.DTOS.DepartmentDTOS;
+using Demo.BusinessLogic.DTOS.EmployeeDTOS;
+using Demo.BusinessLogic.Services.Classes;
+using Demo.BusinessLogic.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.Presentation.Controllers
 {
-    public class EmployeeController(IEmployeeService _employeeService) : Controller
+    public class EmployeeController(IEmployeeService _employeeService
+         , IWebHostEnvironment _env, ILogger<DepartmentController> _logger) : Controller
     {
 
         // Master Avtion
@@ -14,5 +18,46 @@ namespace Demo.Presentation.Controllers
             var employees = _employeeService.GetAllEmployees();
             return View(employees);
         }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CreateEmployeeDto employeeDto)
+        {
+            if (ModelState.IsValid) // Server Side Validation
+            {
+                try
+                {
+                    int result = _employeeService.CreateEmployee(employeeDto);
+                    if (result > 0)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Employee Can not be Created !!");
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (_env.IsDevelopment())
+                    {
+                        _logger.LogError($"Employee Con not be created becouse : {ex.Message}");
+
+                    }
+                    else
+                    {
+                        _logger.LogError($"Employee Con not be created becouse : {ex.Message}");
+                        return View("Error view", ex);
+                    }
+                }
+            }
+            return View(employeeDto);
+
+        }
     }
 }
+
